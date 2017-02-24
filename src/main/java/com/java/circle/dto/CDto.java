36d8ff5132f401d860e20220261867cae08f31a1
sonminhye <smhgood3@gmail.com>
2017-02-24 19:58:46 +0900

    dto add

--- src/main/java/com/java/circle/dto/CDto.java
+++ src/main/java/com/java/circle/dto/CDto.java
@@ -2,7 +2,58 @@ package com.java.circle.dto;
 
 public class CDto {
 	
-	String id;
+	int user_id;
+	String account;
+	String password;
 	String name;
+	int auth_id;
+	String univ;
+	
+	public CDto(int user_id, String account, String password, String name, int auth_id, String univ) {
+		super();
+		this.user_id = user_id;
+		this.account = account;
+		this.password = password;
+		this.name = name;
+		this.auth_id = auth_id;
+		this.univ = univ;
+	}
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAuth_id() {
+		return auth_id;
+	}
+	public void setAuth_id(int auth_id) {
+		this.auth_id = auth_id;
+	}
+	public String getUniv() {
+		return univ;
+	}
+	public void setUniv(String univ) {
+		this.univ = univ;
+	}
 	
 }
