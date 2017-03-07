e3a4f85dde779d8274f90dbcba575713e802e7db
sonminhye <smhgood3@gmail.com>
2017-03-07 16:50:07 +0900

    security error fixed

--- src/main/java/com/java/circle/controller/CController.java
+++ src/main/java/com/java/circle/controller/CController.java
@@ -132,5 +132,19 @@ public class CController {
 		return "addcircle_view";
 	}
 	
+	//시큐리티 로그인과 관련된 맵핑 함수!
+	
+	@RequestMapping(value="/login.html")
+	public String login(Model model){
+		System.out.println("여기에 오긴 올까?");
+		return "security/login";
+	}
+
+	@RequestMapping(value="/welcome.html")
+	public String welcome(Model model){
+		System.out.println("welcome 페이지란다?");
+		return "security/welcome";
+	}
+	
 }
 
